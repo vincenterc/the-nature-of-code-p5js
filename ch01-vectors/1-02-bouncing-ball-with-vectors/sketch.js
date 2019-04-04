5741eca8ba9ccc81a8b88e0a4c58670f32c08d9d
vincenterr <chung.menghsun@gmail.com>
2019-04-04 21:22:40 +0800

    Add 1-02-bouncing-ball-with-vectors

---
+++ ch01-vectors/1-02-bouncing-ball-with-vectors/sketch.js
@@ -0,0 +1,26 @@
+let position;
+let velocity;
+
+function setup() {
+  createCanvas(200, 200);
+  smooth();
+  position = createVector(100, 100);
+  velocity = createVector(2.5, 5);
+}
+
+function draw() {
+  background(255);
+
+  position.add(velocity);
+
+  if (position.x > width || position.x < 0) {
+    velocity.x = velocity.x * -1;
+  }
+  if (position.y > width || position.y < 0) {
+    velocity.y = velocity.y * -1;
+  }
+
+  stroke(0);
+  fill(175);
+  ellipse(position.x, position.y, 16, 16);
+}
