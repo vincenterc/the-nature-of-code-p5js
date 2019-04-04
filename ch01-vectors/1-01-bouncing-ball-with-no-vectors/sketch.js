038240a933eebe2909431bff888bc642cd62ab1c
vincenterr <chung.menghsun@gmail.com>
2019-04-04 21:22:13 +0800

    Add 1-01-bouncing-ball-with-no-vectors

---
+++ ch01-vectors/1-01-bouncing-ball-with-no-vectors/sketch.js
@@ -0,0 +1,28 @@
+let x = 100;
+let y = 100;
+let xSpeed = 1;
+let ySpeed = 3.3;
+
+function setup() {
+  createCanvas(200, 200);
+  smooth();
+  // background(255);
+}
+
+function draw() {
+  background(255);
+
+  x = x + xSpeed;
+  y = y + ySpeed;
+
+  if (x > width || x < 0) {
+    xSpeed = xSpeed * -1;
+  }
+  if (y > width || y < 0) {
+    ySpeed = ySpeed * -1;
+  }
+
+  stroke(0);
+  fill(175);
+  ellipse(x, y, 16, 16);
+}
