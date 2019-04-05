594f94f28d427fee91fe9d30afa653eb549a6e74
vincenterr <chung.menghsun@gmail.com>
2019-04-05 11:39:56 +0800

    Add 1-08-motion-101-velocity-and-constant-acceleration

---
+++ ch01-vectors/1-08-motion-101-velocity-and-constant-acceleration/mover.js
@@ -0,0 +1,34 @@
+class Mover {
+  constructor() {
+    this.location = createVector(width / 2, height / 2);
+    this.velocity = createVector();
+    this.acceleration = createVector(-0.001, 0.01);
+    this.topSpeed = 10;
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.topSpeed);
+    this.location.add(this.velocity);
+  }
+
+  display() {
+    stroke(0);
+    fill(175);
+    ellipse(this.location.x, this.location.y, 16, 16);
+  }
+
+  checkEdges() {
+    if (this.location.x > width) {
+      this.location.x = 0;
+    } else if (this.location.x < 0) {
+      this.location.x = width;
+    }
+
+    if (this.location.y > height) {
+      this.location.y = 0;
+    } else if (this.location.y < 0) {
+      this.location.y = height;
+    }
+  }
+}
