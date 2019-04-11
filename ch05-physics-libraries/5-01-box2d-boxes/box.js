796da0ed0d9d88849663d89b39977157ac64c464
vincenterr <chung.menghsun@gmail.com>
2019-04-11 20:00:28 +0800

    Change names of variables

--- ch05-physics-libraries/5-01-box2d-boxes/box.js
+++ ch05-physics-libraries/5-01-box2d-boxes/box.js
@@ -8,9 +8,9 @@ class Box {
     bd.position = scaleToWorld(x, y);
 
     let ps = new box2d.b2PolygonShape();
-    let box2dw = scaleToWorld(this.w / 2);
-    let box2dh = scaleToWorld(this.h / 2);
-    ps.SetAsBox(box2dw, box2dh);
+    let box2dW = scaleToWorld(this.w / 2);
+    let box2dH = scaleToWorld(this.h / 2);
+    ps.SetAsBox(box2dW, box2dH);
 
     let fd = new box2d.b2FixtureDef();
     fd.shape = ps;
